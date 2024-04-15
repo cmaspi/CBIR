cc99e930f4af361db3c915aea2cc7f54b6b9f294
cmaspi <chiragmehta1010@gmail.com>
2024-04-15 19:14:30 +0530

    uses FAISS library to get the neaster neighbours for each query feature

---
+++ utils/find_similar_vectors.py
@@ -0,0 +1,24 @@
+import faiss
+
+
+class KNN:
+
+    def __init__(self, vec_size: int, mode='cosine', search='exact'):
+        if search == 'exact':
+            if mode == 'cosine':
+                index = faiss.IndexFlatIP(vec_size)
+            elif mode == 'L2':
+                index = faiss.IndexFlatL2(vec_size)
+            else:
+                raise NotImplementedError(f'{mode} not implemented yet')
+        else:
+            raise NotImplementedError(f'{mode} not implemented yet')
+
+        self.index = index
+
+    def fit(self, data):
+        self.index.add(data)
+
+    def findKNearest(self, data, k=10):
+        D, I = self.index.search(data, k)
+        return I
