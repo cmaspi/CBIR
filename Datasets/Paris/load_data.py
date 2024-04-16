b5ff34cf75c30cd8a4f925f302f8911458d31f68
cmaspi <chiragmehta1010@gmail.com>
2024-04-16 12:10:19 +0530

    added load_data file for Paris dataset

---
+++ Datasets/Paris/load_data.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image, ImageOps
+import numpy as np
+from tqdm import tqdm
+
+
+def data_loader():
+    base_directory = os.path.dirname(__file__)
+    parent_dir = base_directory + '/paris/'
+    images = []
+    image_labels = []
+    for child_dir in tqdm(os.listdir(parent_dir)):
+        # if child_dir == 'general':
+            # continue
+        for image_name in tqdm(os.listdir(parent_dir+child_dir), leave=False):
+            image = Image.open(f'{parent_dir}{child_dir}/{image_name}')
+            image = ImageOps.grayscale(image)
+            image = np.array(image)
+            images.append(image)
+            image_labels.append(child_dir)
+    return images, image_labels
