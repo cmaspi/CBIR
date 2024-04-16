2168547832eba42940eece986ad8108fe9095d0f
cmaspi <chiragmehta1010@gmail.com>
2024-04-16 12:10:48 +0530

    __get_features__ now return a n dimension 0 array instead of None

--- utils/get_features.py
+++ utils/get_features.py
@@ -1,17 +1,23 @@
 import cv2
 from typing import List
 from tqdm import tqdm
+import numpy as np
 
 class SIFT:
     def __init__(self, mode='sift'):
         if mode == 'sift':
             self.descriptor_obj = cv2.SIFT_create()
+            self.no_detect_return = np.zeros((1, 128))
         if mode == 'orb':
             self.descriptor_obj = cv2.ORB_create()
+            self.no_detect_return = np.zeros((1, 32))
                     
     def __get_features__(self, image):
         kp, desc = self.descriptor_obj.detectAndCompute(image, None)
-        return desc
+        if desc is not None:
+            return desc
+        else:
+            return self.no_detect_return
     
     def get_features(self, images: List):
         features = []
