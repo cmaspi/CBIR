89606033779e621cfe8bac5dd0849e353adb7de0
Chirag Mehta <chiragmehta1010@gmail.com>
2024-04-26 11:54:18 +0530

    added some documentation

--- utils/cluster.py
+++ utils/cluster.py
@@ -2,7 +2,12 @@ import faiss
 
 
 class KMEANS:
-
+    """
+    Uses FAISS library for KMeans, it will take features,
+    cluster them and return the labels.
+    It is meant to be used to build the histograms each image's
+    descriptors
+    """
     def __init__(self, vec_size, num_clusters=500, niter=100) -> None:
         self.kmeans = faiss.Kmeans(vec_size,
                                    num_clusters,
